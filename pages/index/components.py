6952d58792723534b028666078d3854b9efc1d82
Ashmitban1 <113622309+Ashmitban1@users.noreply.github.com>
2024-11-13 22:01:29 -0600

    Update components.py

--- pages/index/components.py
+++ pages/index/components.py
@@ -7,7 +7,7 @@ import psycopg2
 import os
 
 # PostgreSQL database URL (update if necessary)
-DB_URL = 'postgresql://postgres:feCDeC3CE6e2eCfD43bg11625f1Cg233@autorack.proxy.rlwy.net:30549/railway'
+DB_URL = ''
 
 def get_db():
     """Establish and return a PostgreSQL database connection."""
